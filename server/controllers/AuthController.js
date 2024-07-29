730dff8ca4a6e1b1f3e45780ae3b9ec09add9379
Atul <atulmalviyav2@gmail.com>
2024-07-29 18:24:08 +0530

    Login, userInfo and updateProfile routes and controller, authMiddleware

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const createToken = (email, userId) => {
   return jwt.sign({ email, userId }, process.env.JWT_SECRET, {
@@ -11,25 +12,124 @@ const signUp = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).send("Email and password is required");
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const user = await User.create({ email, password });
     res.cookie("token", createToken(email, user._id), {
       maxAge: 24 * 60 * 60 * 1000,
       secure: true,
       sameSite: "None",
     });
+
     return res.status(201).json({
       user: {
         _id: user._id,
         email: user.email,
         profileSetup: user.profileSetup,
       },
+      message: "User created successfully",
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).send("Internal serval error");
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
-module.exports = { signUp };
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password is required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+    res.cookie("token", createToken(email, user._id), {
+      expiresIn: "1d",
+      secure: true,
+      sameSite: "None",
+    });
+    return res.status(200).json({
+      user: {
+        _id: user._id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
+      },
+      message: "Login successful",
+    });
+  } catch (error) {}
+};
+
+const getUserInfo = async (req, res, next) => {
+  try {
+    const userData = await User.findById(req.userId);
+    if (!userData) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({
+      _id: userData._id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const updateProfile = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    const { firstName, lastName, color } = req.body;
+
+    if (!firstName || !lastName ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findByIdAndUpdate(
+      userId,
+      {
+        firstName,
+        lastName,
+        color,
+        profileSetup: true,
+      },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
+      },
+    });
+  } catch (error) {}
+};
+
+module.exports = { signUp, login, getUserInfo, updateProfile };
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,7 +1,15 @@
 const authRoutes = require("express").Router();
-const authController = require("../controllers/AuthController.js");
+const {
+  signUp,
+  login,
+  getUserInfo,
+  updateProfile
+} = require("../controllers/AuthController.js");
+const { verifyToken } = require("../middlewares/AuthMiddleware");
 
-authRoutes.post("/signup", authController.signUp);
-
+authRoutes.post("/signup", signUp);
+authRoutes.post("/login", login);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
 
 module.exports = authRoutes;
